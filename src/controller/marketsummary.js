647294a666fdc93aeb1472a9cd457361553353e9
vikrantchoudhary <vikrantchoudhary@gmail.com>
2021-02-26 17:24:18 +0000

    Fix for currency conversion feature

--- src/controller/marketsummary.js
+++ src/controller/marketsummary.js
@@ -1,4 +1,7 @@
 const { getMarketSummary } = require("../service/marketsummary-service");
+const { convertCurrency } = require("../service/currency-service");
+const { getLocaleFromRequest,localeIsValid } = require("../util/helpers");
+const boom = require("@hapi/boom");
 
 const marketsummary = (server) => {
     server.route({
@@ -6,8 +9,13 @@ const marketsummary = (server) => {
         path: "/marketsummary",
         handler: async (request, h) => {
             const marketsummary = await getMarketSummary();
+            const locale = getLocaleFromRequest(request);
 
-            return marketsummary;
+            if (localeIsValid(locale)) {
+                return convertCurrency(marketsummary, locale);
+            }
+
+            return boom.badRequest(`received invalid locale: ${locale}`);
         },
     });
 };
